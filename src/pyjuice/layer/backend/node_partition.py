8a6fe9dba1094bf64973c983ee82b98056d625be
Anji Liu <anjiliu219@gmail.com>
2023-03-20 10:59:24 +0800

    add tests for node_partition

--- src/pyjuice/layer/backend/node_partition.py
+++ src/pyjuice/layer/backend/node_partition.py
@@ -5,31 +5,31 @@ import numba
 from typing import Union
 
 
-@numba.jit(nopython = False)
+@numba.jit(forceobj = True)
 def _partition_nodes_dp_simple(nodes_n_edges: np.ndarray, dp: np.ndarray, backtrace: np.ndarray, max_num_groups: int):
     num_nodes = nodes_n_edges.shape[0]
 
     # Init
-    dp[:,1] = nodes_n_edges * np.arange(1, num_nodes)
+    dp[:,1] = nodes_n_edges * np.arange(1, num_nodes + 1)
 
     # Main DP
     for n_group in range(2, max_num_groups + 1):
         dp[0,n_group] = nodes_n_edges[0]
         backtrace[0,n_group] = 0
-        for i in range(1, len(num_nodes)):
+        for i in range(1, num_nodes):
             min_overhead = 2 ** 31 - 1
             best_idx = -1
             for j in range(i):
-                curr_overhead = dp[j,n_group-1] + nodes_n_edges[i] * (j - i)
+                curr_overhead = dp[j,n_group-1] + nodes_n_edges[i] * (i - j)
                 if curr_overhead < min_overhead:
                     min_overhead = curr_overhead
                     best_idx = j
 
             dp[i,n_group] = min_overhead
-            backtrace[i,n_group] = j
+            backtrace[i,n_group] = best_idx
 
     # Choose number of groups
-    n_group = np.min(np.where(dp[-1,:] >= dp[-1,-1] * 0.95)[0])
+    n_group = np.min(np.where(dp[-1,1:] <= dp[-1,-1] * 1.05)[0]) + 1
 
     # Backtrace
     partitions = np.zeros([n_group], dtype = np.int64)
@@ -38,7 +38,7 @@ def _partition_nodes_dp_simple(nodes_n_edges: np.ndarray, dp: np.ndarray, backtr
         partitions[n-1] = i
         i = backtrace[i,n_group]
 
-    return np.unique(partitions)
+    return np.unique(nodes_n_edges[partitions])
 
 
 def partition_nodes_by_n_edges(node_n_edges: Union[np.ndarray, torch.Tensor], max_num_groups: int, algorithm: str = "dp_simple"):
