3db5dfcdb516f3cb09de77678fbafd6cd1aaa2cc
Anji Liu <anjiliu219@gmail.com>
2023-06-11 17:00:04 +0800

    copy circuit with parameter tying

--- src/pyjuice/transformations/copy.py
+++ src/pyjuice/transformations/copy.py
@@ -1,16 +1,19 @@
 from __future__ import annotations
 
-from copy import deepcopy
+from copy import deepcopy as pydeepcopy
+from typing import Optional, Dict
 
 from pyjuice.nodes import CircuitNodes, InputNodes, ProdNodes, SumNodes
+from pyjuice.utils import BitSet
 
 
-def deepcopy(root_nodes: CircuitNodes):
+def deepcopy(root_nodes: CircuitNodes, tie_params: bool = False, 
+             var_mapping: Optional[Dict[int,int]] = None):
     old2new = dict()
     tied_ns_pairs = []
 
     def dfs(ns: CircuitNodes):
-        if ns in cache:
+        if ns in old2new:
             return
 
         # Recursively traverse children
@@ -18,36 +21,53 @@ def deepcopy(root_nodes: CircuitNodes):
             for cs in ns.chs:
                 dfs(cs)
 
-        new_chs = [cache[cs] for cs in ns.chs]
+        new_chs = [old2new[cs] for cs in ns.chs]
 
-        if ns.is_tied():
+        if not tie_params and ns.is_tied():
             tied_ns_pairs.append((ns, ns.get_source_ns()))
 
         if ns.issum():
-            new_ns = SumNodes(
-                ns.num_nodes,
-                new_chs,
-                ns.edge_ids.clone()
-            )
-            params = ns.get_params()
-            if params is not None:
-                new_ns.set_params(params.clone())
+            if not tie_params:
+                new_ns = SumNodes(
+                    ns.num_nodes,
+                    new_chs,
+                    ns.edge_ids.clone()
+                )
+                params = ns.get_params()
+                if params is not None:
+                    new_ns.set_params(params.clone())
+            else:
+                new_ns = ns.duplicate(*new_chs, tie_params = True)
+            
         elif ns.isprod():
             new_ns = ProdNodes(
                 ns.num_nodes,
                 new_chs,
                 ns.edge_ids.clone()
             )
+            
         else:
             assert ns.isinput()
-            new_ns = InputNodes(
-                ns.num_nodes,
-                deepcopy(ns.scope),
-                deepcopy(ns.dist)
-            )
-            params = ns.get_params()
-            if params is not None:
-                new_ns.set_params(params.clone())
+            if not tie_params:
+                new_ns = InputNodes(
+                    ns.num_nodes,
+                    pydeepcopy(ns.scope),
+                    pydeepcopy(ns.dist)
+                )
+                params = ns.get_params()
+                if params is not None:
+                    new_ns.set_params(params.clone())
+            else:
+                if var_mapping is not None:
+                    ns_scope = ns.scope
+                    scope = BitSet()
+                    for v in ns_scope:
+                        assert v in var_mapping, f"Variable {v} not found in `var_mapping`."
+                        scope.add(var_mapping[v])
+                else:
+                    scope = pydeepcopy(ns.scope)
+
+                new_ns = ns.duplicate(scope = scope, tie_params = True)
 
         old2new[ns] = new_ns
 
