a3bf1dcba101dbf7bfd04af09028744eaa36dc00
Anji Liu <anjiliu219@gmail.com>
2023-11-05 23:52:18 +0800

    fix dependency

--- src/pyjuice/layer/__init__.py
+++ src/pyjuice/layer/__init__.py
@@ -2,4 +2,3 @@ from .layer import Layer
 from .input_layer import InputLayer
 from .prod_layer import ProdLayer
 from .sum_layer import SumLayer
-from .input_layers import *
--- src/pyjuice/structures/hmm.py
+++ src/pyjuice/structures/hmm.py
@@ -6,11 +6,12 @@ from typing import Type
 from pyjuice.graph import *
 from pyjuice.layer import *
 from pyjuice.model import TensorCircuit
+from pyjuice.nodes.distributions import Distribution, Categorical
 from .compilation import BayesianTreeToHiddenRegionGraph
 
 
 def HMM(length: int, num_latents: int, 
-        input_layer_type: Type[InputLayer] = CategoricalLayer, 
+        input_layer_type: Type[Distribution] = Categorical, 
         input_layer_params: dict = {"num_cats": 256}):
 
     # The Graph is just a path x_1 => x_2 ..... => x_n
