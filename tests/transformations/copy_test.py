ace4488253428def4f663467b22c607086fd01f9
Anji Liu <anjiliu219@gmail.com>
2023-12-01 03:22:33 +0800

    refactor: `deepcopy`

--- tests/transformations/copy_test.py
+++ tests/transformations/copy_test.py
@@ -11,52 +11,58 @@ import pytest
 
 
 def copy_test():
-    num_nodes = 2
+    num_node_groups_candidates = [2, 4, 7]
+    group_size_candidates = [1, 4, 8]
 
-    i00 = inputs(0, num_nodes, dists.Categorical(num_cats = 5))
-    i10 = inputs(1, num_nodes, dists.Categorical(num_cats = 5))
-    i11 = inputs(1, num_nodes, dists.Categorical(num_cats = 5))
-    
-    ms0 = multiply(i00, i10)
-    ms1 = multiply(i00, i11)
+    for num_node_groups in num_node_groups_candidates:
+        for group_size in group_size_candidates:
 
-    ns = summate(ms0, ms1, num_nodes = 1)
+            with juice.set_group_size(group_size):
 
-    ns.init_parameters()
+                i00 = inputs(0, num_node_groups, dists.Categorical(num_cats = 5))
+                i10 = inputs(1, num_node_groups, dists.Categorical(num_cats = 5))
+                i11 = inputs(1, num_node_groups, dists.Categorical(num_cats = 5))
+                
+                ms0 = multiply(i00, i10)
+                ms1 = multiply(i00, i11)
 
-    ## Copy without parameter tying ##
+                ns = summate(ms0, ms1, num_node_groups = 1)
 
-    new_ns = deepcopy(ns)
+                ns.init_parameters()
 
-    assert ns.num_nodes == new_ns.num_nodes
-    assert torch.all(ns.edge_ids == new_ns.edge_ids)
-    assert torch.all(ns._params == new_ns._params)
+                ## Copy without parameter tying ##
 
-    assert torch.all(ns.chs[0].edge_ids == new_ns.chs[0].edge_ids)
-    assert torch.all(ns.chs[1].edge_ids == new_ns.chs[1].edge_ids)
+                new_ns = deepcopy(ns)
 
-    assert new_ns.chs[0].chs[0] == new_ns.chs[1].chs[0]
-    assert torch.all((ns.chs[0].chs[0].get_params() - new_ns.chs[0].chs[0].get_params()).abs() < 1e-6)
-    assert torch.all((ns.chs[0].chs[1].get_params() - new_ns.chs[0].chs[1].get_params()).abs() < 1e-6)
-    assert torch.all((ns.chs[1].chs[1].get_params() - new_ns.chs[1].chs[1].get_params()).abs() < 1e-6)
+                assert ns.num_nodes == new_ns.num_nodes
+                assert torch.all(ns.edge_ids == new_ns.edge_ids)
+                assert torch.all(ns._params == new_ns._params)
 
-    ## Copy with parameter tying ##
+                assert torch.all(ns.chs[0].edge_ids == new_ns.chs[0].edge_ids)
+                assert torch.all(ns.chs[1].edge_ids == new_ns.chs[1].edge_ids)
 
-    new_ns = deepcopy(ns, tie_params = True, var_mapping = {0: 2, 1: 3})
+                assert new_ns.chs[0].chs[0] == new_ns.chs[1].chs[0]
+                assert torch.all((ns.chs[0].chs[0].get_params() - new_ns.chs[0].chs[0].get_params()).abs() < 1e-6)
+                assert torch.all((ns.chs[0].chs[1].get_params() - new_ns.chs[0].chs[1].get_params()).abs() < 1e-6)
+                assert torch.all((ns.chs[1].chs[1].get_params() - new_ns.chs[1].chs[1].get_params()).abs() < 1e-6)
 
-    assert ns.num_nodes == new_ns.num_nodes
-    assert torch.all(ns.edge_ids == new_ns.edge_ids)
-    assert new_ns.get_source_ns() == ns
+                ## Copy with parameter tying ##
 
-    assert torch.all(ns.chs[0].edge_ids == new_ns.chs[0].edge_ids)
-    assert torch.all(ns.chs[1].edge_ids == new_ns.chs[1].edge_ids)
+                new_ns = deepcopy(ns, tie_params = True, var_mapping = {0: 2, 1: 3})
 
-    assert new_ns.chs[0].chs[0].get_source_ns() == ns.chs[0].chs[0]
-    assert new_ns.chs[0].chs[1].get_source_ns() == ns.chs[0].chs[1]
-    assert new_ns.chs[1].chs[1].get_source_ns() == ns.chs[1].chs[1]
-    assert tuple(new_ns.chs[0].chs[0].scope.to_list()) == (2,)
-    assert tuple(new_ns.chs[0].chs[1].scope.to_list()) == (3,)
-    assert tuple(new_ns.chs[1].chs[1].scope.to_list()) == (3,)
+                assert ns.num_nodes == new_ns.num_nodes
+                assert torch.all(ns.edge_ids == new_ns.edge_ids)
+                assert new_ns.get_source_ns() == ns
+
+                assert torch.all(ns.chs[0].edge_ids == new_ns.chs[0].edge_ids)
+                assert torch.all(ns.chs[1].edge_ids == new_ns.chs[1].edge_ids)
+
+                assert new_ns.chs[0].chs[0].get_source_ns() == ns.chs[0].chs[0]
+                assert new_ns.chs[0].chs[1].get_source_ns() == ns.chs[0].chs[1]
+                assert new_ns.chs[1].chs[1].get_source_ns() == ns.chs[1].chs[1]
+                assert tuple(new_ns.chs[0].chs[0].scope.to_list()) == (2,)
+                assert tuple(new_ns.chs[0].chs[1].scope.to_list()) == (3,)
+                assert tuple(new_ns.chs[1].chs[1].scope.to_list()) == (3,)
 
 
 if __name__ == "__main__":
