f89a87ec9d28496d8e11e6bc9b5a5fc63c818348
Anji Liu <anjiliu219@gmail.com>
2023-12-26 15:25:45 +0800

    temp update

--- tests/structures/debug.py
+++ tests/structures/debug.py
@@ -102,11 +102,11 @@ def ref_kernel(node_flows, element_flows, node_mars, element_mars, params,
 
 
 @triton.jit
-def my_kernel(aaa, bbb, ccc, ddd, eee, node_flows, element_flows, node_mars, element_mars, params, 
-                                        chids, parids_start, parids_increment, parpids_start, parpids_increment, 
-                                        local_ids, batch_size: tl.constexpr, partial_eval: tl.constexpr, ptr_inc_step: tl.constexpr, 
-                                        BLOCK_B: tl.constexpr, TILE_SIZE_K: tl.constexpr, K_NUM_TILES: tl.constexpr, TILE_SIZE_M: tl.constexpr, 
-                                        GROUP_SIZE_M: tl.constexpr, GROUP_SIZE_K: tl.constexpr):
+def my_kernel(aaa, bbb, ccc, node_flows, element_flows, node_mars, element_mars, params, 
+            chids, parids_start, parids_increment, parpids_start, parpids_increment, 
+            local_ids, batch_size: tl.constexpr, partial_eval: tl.constexpr, ptr_inc_step: tl.constexpr, 
+            BLOCK_B: tl.constexpr, TILE_SIZE_K: tl.constexpr, K_NUM_TILES: tl.constexpr, TILE_SIZE_M: tl.constexpr, 
+            GROUP_SIZE_M: tl.constexpr, GROUP_SIZE_K: tl.constexpr):
 
     pid_b = tl.program_id(0) # ID of size-`BLOCK_B` batches
     pid_m = tl.program_id(1) # ID of size-`TILE_SIZE_M` nodes
@@ -129,11 +129,6 @@ def my_kernel(aaa, bbb, ccc, ddd, eee, node_flows, element_flows, node_mars, ele
         offs_ele[:,None] * GROUP_SIZE_K + \
         (par_start + offs_edge_nid)[None,:] # [TILE_SIZE_M, TILE_SIZE_K]
 
-    epars = tl.load(epars_ptr)
-    offs1 = pid_m * (TILE_SIZE_M * TILE_SIZE_K) + tl.arange(0, TILE_SIZE_M)[:,None] * TILE_SIZE_K + tl.arange(0, TILE_SIZE_K)[None,:]
-    tl.store(aaa + offs1, epars)
-    tl.store(bbb + offs1, offs_ele[:,None] * GROUP_SIZE_K + (par_start + offs_edge_nid)[None,:])
-
     # Batch offsets and mask
     offs_batch = tl.arange(0, BLOCK_B) + pid_b * BLOCK_B
     mask_batch = offs_batch < batch_size
@@ -153,34 +148,35 @@ def my_kernel(aaa, bbb, ccc, ddd, eee, node_flows, element_flows, node_mars, ele
 
     # Inner loop
     acc = tl.zeros([TILE_SIZE_M, BLOCK_B], dtype = tl.float32) - float("inf")
-    # acc = tl.zeros([TILE_SIZE_M, BLOCK_B], dtype = tl.float32)
 
-    for k in range(0, K_NUM_TILES):
-    # for k in range(0, 1):
+    # for k in range(0, K_NUM_TILES):
+    for k in range(0, 1):
         epars = tl.load(epars_ptr) # [TILE_SIZE_M, TILE_SIZE_K]
         nmars = tl.load(nmars_ptr, mask = mask_batch[None,:]) # [TILE_SIZE_K, BLOCK_B]
         nflows = tl.load(nflows_ptr, mask = mask_batch[None,:]) # [TILE_SIZE_K, BLOCK_B]
 
         log_n_fdm = tl.log(nflows) - nmars
         log_n_fdm_max = tl.max(log_n_fdm, axis = 0)
-        n_fdm_sub = tl.where(log_n_fdm_max[None,:] != -float("inf"), 
-            tl.exp(log_n_fdm - log_n_fdm_max[None,:]), 0.0)
+        n_fdm_sub = tl.where(log_n_fdm_max[None,:] != -float("inf"), tl.exp(log_n_fdm - log_n_fdm_max[None,:]), 0.0)
 
-        offs2 = pid_m * (K_NUM_TILES * TILE_SIZE_K * batch_size) + k * (TILE_SIZE_K * batch_size) + tl.arange(0, TILE_SIZE_K)[:,None] * batch_size + offs_batch[None,:]
-        tl.store(ccc + offs2, log_n_fdm, mask = mask_batch[None,:])
-        tl.store(ddd + offs2, n_fdm_sub, mask = mask_batch[None,:])
+        offs_aaa = pid_m * (TILE_SIZE_K * batch_size) + tl.arange(0, TILE_SIZE_K)[:,None] * batch_size + offs_batch[None,:]
+        tl.store(aaa + offs_aaa, n_fdm_sub, mask = mask_batch[None,:])
 
         partial_flows = tl.dot(epars, n_fdm_sub)
         # partial_flows = tl.sum(epars[:,:,None] * n_fdm_sub[None,:,:], axis = 1)
 
-        offs3 = pid_m * (K_NUM_TILES * TILE_SIZE_K * batch_size) + k * (TILE_SIZE_K * batch_size) + tl.arange(0, TILE_SIZE_M)[:,None] * batch_size + offs_batch[None,:]
-        tl.store(eee + offs3, partial_flows, mask = mask_batch[None,:])
+        offs_bbb = pid_m * (TILE_SIZE_M * batch_size) + tl.arange(0, TILE_SIZE_M)[:,None] * batch_size + offs_batch[None,:]
+        tl.store(bbb + offs_bbb, partial_flows, mask = mask_batch[None,:])
 
+        offs_ccc = pid_m * batch_size + offs_batch
+        tl.store(ccc + offs_ccc, log_n_fdm_max, mask = mask_batch)
+
+        neginf_flag = (log_n_fdm_max[None,:] == -float("inf")) & (acc == -float("inf"))
         acc = tl.where(log_n_fdm_max[None,:] > acc,
             tl.log(partial_flows + tl.exp(acc - log_n_fdm_max[None,:])) + log_n_fdm_max[None,:],
             tl.log(tl.exp(log_n_fdm_max[None,:] - acc) * partial_flows + 1.0) + acc
         )
-        # acc += partial_flows
+        acc = tl.where(neginf_flag, -float("inf"), acc)
 
         # Increment `epars_ptr`
         parpids_inc = tl.load(parpids_inc_ptr)
@@ -199,7 +195,6 @@ def my_kernel(aaa, bbb, ccc, ddd, eee, node_flows, element_flows, node_mars, ele
     emars = tl.load(emars_ptr, mask = mask_batch[None,:]) # [TILE_SIZE_K, BLOCK_B]
 
     eflows = tl.exp(acc + emars)
-    # eflows = acc
 
     # Write back
     offs_elemfs = (off_eleids + offs_ele[:,None]) * batch_size + offs_batch[None,:]
@@ -213,7 +208,7 @@ def main():
     data = np.load("temp.npz")
 
     node_flows = torch.from_numpy(data["node_flows"]).to(device)
-    element_flows = torch.from_numpy(data["element_flow"]).to(device)
+    element_flows = torch.from_numpy(data["element_flows"]).to(device)
     node_mars = torch.from_numpy(data["node_mars"]).to(device)
     element_mars = torch.from_numpy(data["element_mars"]).to(device)
     params = torch.from_numpy(data["params"]).to(device)
@@ -232,39 +227,11 @@ def main():
     K_NUM_TILES = int(data["K_NUM_TILES"])
     GROUP_SIZE_M = int(data["GROUP_SIZE_M"])
     GROUP_SIZE_K = int(data["GROUP_SIZE_K"])
-    OP_MODE = int(data["OP_MODE"])
     layer_n_nodes = int(data["layer_n_nodes"])
 
-    # node_flows = torch.rand(node_flows.size(), device = device)
-
     grid = (triton.cdiv(batch_size, BLOCK_B), triton.cdiv(layer_n_nodes, TILE_SIZE_M))
-    # grid = (1, triton.cdiv(layer_n_nodes, TILE_SIZE_M))
 
-    # ref_kernel[grid](
-    #     node_flows = node_flows, 
-    #     element_flows = element_flows, 
-    #     node_mars = node_mars, 
-    #     element_mars = element_mars, 
-    #     params = params, 
-    #     chids = chids, 
-    #     parids_start = parids_start,
-    #     parids_increment = parids_increment,
-    #     parpids_start = parpids_start,
-    #     parpids_increment = parpids_increment, 
-    #     local_ids = None, 
-    #     batch_size = batch_size, 
-    #     partial_eval = 0,
-    #     ptr_inc_step = ptr_inc_step,
-    #     BLOCK_B = BLOCK_B, 
-    #     TILE_SIZE_K = TILE_SIZE_K, 
-    #     K_NUM_TILES = K_NUM_TILES,
-    #     TILE_SIZE_M = TILE_SIZE_M, 
-    #     GROUP_SIZE_M = GROUP_SIZE_M,
-    #     GROUP_SIZE_K = GROUP_SIZE_K
-    # )
-
-    aaa = ref_kernel[grid]
-    aaa(
+    ref_kernel[grid](
         node_flows = node_flows, 
         element_flows = element_flows, 
         node_mars = node_mars, 
@@ -289,20 +256,24 @@ def main():
 
     torch.cuda.synchronize()
 
+    import pdb; pdb.set_trace()
+
     element_flows_ref = element_flows.clone()
 
-    aaa = torch.zeros([grid[1], TILE_SIZE_M, TILE_SIZE_K]).cuda()
-    bbb = torch.zeros([grid[1], TILE_SIZE_M, TILE_SIZE_K], dtype = torch.long).cuda()
-    ccc = torch.zeros([grid[1], K_NUM_TILES, TILE_SIZE_K, batch_size]).cuda()
-    ddd = torch.zeros([grid[1], K_NUM_TILES, TILE_SIZE_K, batch_size]).cuda()
-    eee = torch.zeros([grid[1], K_NUM_TILES, TILE_SIZE_M, batch_size]).cuda()
+    # aaa = torch.zeros([grid[1], TILE_SIZE_M, TILE_SIZE_K]).cuda()
+    # bbb = torch.zeros([grid[1], TILE_SIZE_M, TILE_SIZE_K], dtype = torch.long).cuda()
+    # ccc = torch.zeros([grid[1], K_NUM_TILES, TILE_SIZE_K, batch_size]).cuda()
+    # ddd = torch.zeros([grid[1], K_NUM_TILES, TILE_SIZE_K, batch_size]).cuda()
+    # eee = torch.zeros([grid[1], K_NUM_TILES, TILE_SIZE_M, batch_size]).cuda()
+
+    aaa = torch.zeros([grid[1], TILE_SIZE_K, batch_size]).cuda()
+    bbb = torch.zeros([grid[1], TILE_SIZE_M, batch_size]).cuda()
+    ccc = torch.zeros([grid[1], batch_size]).cuda()
 
     my_kernel[grid](
         aaa = aaa,
         bbb = bbb,
         ccc = ccc,
-        ddd = ddd,
-        eee = eee,
         node_flows = node_flows, 
         element_flows = element_flows, 
         node_mars = node_mars, 
