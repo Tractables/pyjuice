809080de47e7f4592eecc3410a1bba8796004317
Anji Liu <anjiliu219@gmail.com>
2023-12-30 21:38:50 +0800

    finish `group` function

--- src/pyjuice/transformations/group.py
+++ src/pyjuice/transformations/group.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+from copy import deepcopy as pydeepcopy
 from typing import Optional, Dict, Sequence
 
 from pyjuice.nodes import CircuitNodes, InputNodes, ProdNodes, SumNodes
@@ -96,13 +97,113 @@ def group(root_ns: CircuitNodes, sparsity_tolerance: float = 0.25, max_target_gr
     ## Apply the new group sizes ##
 
     def update_ns(ns: CircuitNodes, ns_chs: Sequence[CircuitNodes]):
+        new_group_size = ns2group_size[ns]
+        group_mul_size = new_group_size // ns.group_size
+
+        new_num_ngroups = ns.num_node_groups // group_mul_size
+
         if ns.isinput():
-            pass
+            new_ns = InputNodes(
+                num_node_groups = new_num_ngroups,
+                scope = pydeepcopy(scope),
+                dist = pydeepcopy(ns.dist),
+                group_size = new_group_size
+            )
+
+            if not ns.is_tied():
+                params = ns.get_params()
+                if params is not None:
+                    new_ns.set_params(params.clone(), normalize = False)
 
         elif ns.isprod():
-            pass
+            edge_ids = ns.edge_ids.clone()
+            edge_ids = edge_ids.reshape(new_num_ngroups, group_mul_size, ns.num_chs)
+            if torch.all(edge_ids[:,1:,:] - edge_ids[:,:-1,:]) == 1:
+                # Block-sparse mode
+                edge_ids = edge_ids[:,0,:].contiguous()
+                mode = "block_sparse"
+            else:
+                # Sparse mode
+                edge_ids = (edge_ids.reshape(ns.group_size, ns.num_chs)[:,None,:] * ns.group_size + \
+                    torch.arange(0, ns.group_size)[None,:,None]).flatten(0, 1)
+                mode = "sparse"
+            
+            new_ns = ProdNodes(
+                num_node_groups = new_num_ngroups,
+                chs = ns_chs,
+                edge_ids = edge_ids,
+                group_size = ns.group_size
+            )
+
+            if mode == "block_sparse":
+                assert new_ns.is_block_sparse
+            elif mode == "sparse":
+                assert new_ns.is_sparse
 
         else:
             assert ns.issum()
 
-    return foldup_aggregate(update_ns, root_ns)
+            old_num_ngroups = ns.num_node_groups
+            old_num_cgroups = sum([cs.num_node_groups for cs in ns.chs])
+
+            new_cs_group_size = ns2group_size[ns.chs[0]]
+            cs_group_mul_size = new_cs_group_size // ns.chs[0].group_size
+
+            new_num_cgroups = old_num_cgroups // cs_group_mul_size
+
+            edge_ids = ns.edge_ids.clone()
+            grid_edge_ids = torch.zeros([old_num_ngroups, old_num_cgroups], dtype = torch.bool)
+            grid_edge_ids[edge_ids[0,:],edge_ids[1,:]] = True
+
+            grid_edge_ids = grid_edge_ids.reshape(new_num_ngroups, group_mul_size, new_num_cgroups, cs_group_mul_size)
+            new_edge_ids = torch.nonzero(grid_edge_ids.any(dim = 3).any(dim = 1), as_tuple = False).permute(1, 0)
+
+            new_ns = SumNodes(
+                num_node_groups = new_num_ngroups,
+                chs = new_chs,
+                edge_ids = new_edge_ids,
+                group_size = ns.group_size
+            )
+            
+            if not ns.is_tied():
+                # Collect selected blocks
+                grid_edge_ids = grid_edge_ids.permute(0, 2, 1, 3).flatten(0, 1)
+                block_ids = new_edge_ids[0,:] * new_num_cgroups + new_edge_ids[1,:]
+                param_indicator = grid_edge_ids[block_ids,:,:]
+                param_indicator = param_indicator[:,:,None,:,None].repeat(1, 1, ns.group_size, 1, ns.chs[0].group_size)
+                param_indicator = param_indicator.flatten(3, 4).flatten(1, 2)
+                zero_param_mask = ~param_indicator
+
+                new_ns.set_zero_param_mask(zero_param_mask)
+
+                params = ns.get_params()
+                if params is not None:
+                    # TODO: add a GPU implementation
+                    new_params = torch.zeros([new_edge_ids.size(1), new_group_size, new_cs_group_size])
+                    for par_group_id in range(new_edge_ids.size(1)):
+                        nsid = new_edge_ids[0,i] * group_mul_size
+                        neid = nsid + group_mul_size
+                        csid = new_edge_ids[1,i] * cs_group_mul_size
+                        ceid = csid + cs_group_mul_size
+
+                        blk_ids = torch.where((nsid <= edge_ids[0,:] < neid) & (csid <= edge_ids[1,:] < ceid))
+                        for blk_id in blk_ids:
+                            nid0, nid1 = (edge_ids[0,:] - nsid) * ns.group_size, (edge_ids[0,:] - nsid + 1) * ns.group_size
+                            cid0, cid1 = (edge_ids[1,:] - csid) * ns.chs[0].group_size, (edge_ids[1,:] - csid + 1) * ns.chs[0].group_size
+                            new_params[par_group_id,nid0:nid1,cid0:cid1] = params[blk_id,:,:]
+
+                    new_ns.set_params(params.clone(), normalize = False)
+
+        return new_ns
+
+    old2new = dict()
+    new_root_ns = foldup_aggregate(update_ns, root_ns, cache = old2new)
+
+    # Re-link tied nodes to their source
+    for ns in root_ns:
+        if ns.is_tied():
+            new_source_ns = old2new[ns.get_source_ns()]
+            new_ns = old2new[ns]
+            new_ns.set_source_ns(new_source_ns)
+
+    return new_root_ns
