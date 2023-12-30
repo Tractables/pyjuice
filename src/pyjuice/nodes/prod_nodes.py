38ce2bc7bd1d4479b1427937ab732fd49f71567b
Anji Liu <anjiliu219@gmail.com>
2023-12-30 17:04:07 +0800

    add "sparse edge" mode for product nodes

--- src/pyjuice/nodes/prod_nodes.py
+++ src/pyjuice/nodes/prod_nodes.py
@@ -13,6 +13,10 @@ Tensor = Union[np.ndarray,torch.Tensor]
 
 
 class ProdNodes(CircuitNodes):
+
+    SPARSE = 0
+    BLOCK_SPARSE = 1
+
     def __init__(self, num_node_groups: int, chs: Sequence[CircuitNodes], edge_ids: Optional[Tensor] = None, group_size: int = 0, **kwargs) -> None:
 
         rg_node = PartitionNode([ch.region_node for ch in chs])
@@ -27,28 +31,26 @@ class ProdNodes(CircuitNodes):
         # Callbacks
         self._run_init_callbacks(**kwargs)
 
-    def _construct_edges(self, edge_ids: Optional[Tensor]):
-        if edge_ids is None:
-            for c in self.chs:
-                assert self.num_node_groups == c.num_node_groups and self.group_size == c.group_size, \
-                    "Cannot create edges implicitly since # nodes do not match."
-
-            edge_ids = torch.arange(self.num_node_groups).unsqueeze(1).repeat(1, self.num_chs)
-
-        if isinstance(edge_ids, np.ndarray):
-            edge_ids = torch.from_numpy(edge_ids)
-
-        # Sanity checks
-        assert edge_ids.size(0) == self.num_node_groups and edge_ids.size(1) == self.num_chs, f"Expect edge_ids.size() == ({self.num_node_groups}, {self.num_chs})."
-        for cid in range(self.num_chs):
-            assert torch.all(edge_ids[:,cid] >= 0), "Edge index underflow."
-            assert torch.all(edge_ids[:,cid] < self.chs[cid].num_node_groups), "Edge index overflow."
-
-        self.edge_ids = edge_ids
-
     @property
     def num_edges(self):
-        return self.edge_ids.size(0) * self.edge_ids.size(1) * self.group_size
+        return self.num_nodes * self.num_chs
+
+    @property
+    def edge_type(self):
+        if self.edge_ids.size(0) == self.num_node_groups:
+            return self.BLOCK_SPARSE
+        elif self.edge_ids.size(0) == self.num_nodes:
+            return self.SPARSE
+        else:
+            raise RuntimeError(f"Unexpected shape of `edge_ids`: ({self.edge_ids.size(0)}, {self.edge_ids.size(1)})")
+
+    @property
+    def is_block_sparse(self):
+        return self.edge_type == self.BLOCK_SPARSE
+
+    @property
+    def is_sparse(self):
+        return self.edge_type == self.SPARSE
 
     def duplicate(self, *args, tie_params: bool = False):
         chs = []
@@ -78,4 +80,32 @@ class ProdNodes(CircuitNodes):
         )
 
     def __repr__(self):
-        return f"ProdNodes(num_node_groups={self.num_node_groups}, group_size={self.group_size}, num_chs={self.num_chs})"
+        edge_type = "sparse" if self.edge_type == self.SPARSE else "block_sparse"
+        return f"ProdNodes(num_node_groups={self.num_node_groups}, group_size={self.group_size}, num_chs={self.num_chs}, edge_type='{edge_type}')"
+
+    def _construct_edges(self, edge_ids: Optional[Tensor]):
+        if edge_ids is None:
+            for c in self.chs:
+                assert self.num_node_groups == c.num_node_groups and self.group_size == c.group_size, \
+                    "Cannot create edges implicitly since # nodes do not match."
+
+            edge_ids = torch.arange(self.num_node_groups).unsqueeze(1).repeat(1, self.num_chs)
+
+        if isinstance(edge_ids, np.ndarray):
+            edge_ids = torch.from_numpy(edge_ids)
+
+        # Sanity checks
+        if edge_ids.size(0) == self.num_node_groups:
+            assert edge_ids.size(0) == self.num_node_groups and edge_ids.size(1) == self.num_chs, f"Expect edge_ids.size() == ({self.num_node_groups}, {self.num_chs})."
+            for cid in range(self.num_chs):
+                assert torch.all(edge_ids[:,cid] >= 0), "Edge index underflow."
+                assert torch.all(edge_ids[:,cid] < self.chs[cid].num_node_groups), "Edge index overflow."
+        elif edge_ids.size(0) == self.num_nodes:
+            assert edge_ids.size(0) == self.num_nodes and edge_ids.size(1) == self.num_chs, f"Expect edge_ids.size() == ({self.num_nodes}, {self.num_chs})."
+            for cid in range(self.num_chs):
+                assert torch.all(edge_ids[:,cid] >= 0), "Edge index underflow."
+                assert torch.all(edge_ids[:,cid] < self.chs[cid].num_nodes), "Edge index overflow."
+        else:
+            raise RuntimeError(f"Unexpected shape of `edge_ids`: ({self.edge_ids.size(0)}, {self.edge_ids.size(1)})")
+
+        self.edge_ids = edge_ids
