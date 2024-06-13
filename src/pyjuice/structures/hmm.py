b37a236b1f16184f3d2669768a397c01999e89d5
Anji Liu <anjiliu219@gmail.com>
2024-06-13 15:27:52 +0800

    remove old hmm implementation

--- src/pyjuice/structures/hmm.py
+++ src/pyjuice/structures/hmm.py
@@ -12,24 +12,6 @@ from pyjuice.utils.util import max_cdf_power_of_2
 from .compilation import BayesianTreeToHiddenRegionGraph
 
 
-def HMM(length: int, num_latents: int, 
-        input_layer_type: Type[Distribution] = Categorical, 
-        input_layer_params: dict = {"num_cats": 256}):
-
-    # The Graph is just a path x_1 => x_2 ..... => x_n
-    N = length
-    T = nx.Graph()
-    for v in range(N):
-        T.add_node(v)
-        if v > 0:
-            T.add_edge(v-1, v)
-
-    root = 0
-    root_r = BayesianTreeToHiddenRegionGraph(T, root, num_latents, input_layer_type, input_layer_params)
-    
-    return root_r
-
-
 def HMM(seq_length: int, num_latents: int, num_emits: int, homogeneous: bool = True):
     """
     Constructs Hidden Markov Models.
