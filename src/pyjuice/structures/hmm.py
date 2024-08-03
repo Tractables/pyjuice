d196b6da04510a35e0d6a2e17995600cb985f3b7
Anji Liu <anjiliu219@gmail.com>
2024-08-03 17:50:51 +0000

    api for hmm init params

--- src/pyjuice/structures/hmm.py
+++ src/pyjuice/structures/hmm.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 import torch
 import networkx as nx
-from typing import Type
+from typing import Type, Optional
 from pyjuice.graph import *
 from pyjuice.layer import *
 from pyjuice.model import TensorCircuit
@@ -12,7 +12,8 @@ from pyjuice.utils.util import max_cdf_power_of_2
 from .compilation import BayesianTreeToHiddenRegionGraph
 
 
-def HMM(seq_length: int, num_latents: int, num_emits: int, homogeneous: bool = True):
+def HMM(seq_length: int, num_latents: int, num_emits: int, homogeneous: bool = True,
+        alpha: Optional[torch.Tensor] = None, beta: Optional[torch.Tensor] = None, gamma: Optional[torch.Tensor] = None):
     """
     Constructs Hidden Markov Models.
 
@@ -27,6 +28,15 @@ def HMM(seq_length: int, num_latents: int, num_emits: int, homogeneous: bool = T
 
     :param homogeneous: whether to define a homogeneous (or inhomogeneous) HMM
     :type homogeneous: bool
+
+    :param alpha: optional transition parameters of size `[num_latents, num_latents]`
+    :type alpha: Optional[torch.Tensor]
+
+    :param beta: optional emission parameters of size `[num_latents, num_emits]`
+    :type beta: Optional[torch.Tensor]
+
+    :param gamma: optional init parameters of size `[num_latents]`
+    :type gamma: Optional[torch.Tensor]
     """
     
     block_size = min(max_cdf_power_of_2(num_latents), 1024)
@@ -38,6 +48,10 @@ def HMM(seq_length: int, num_latents: int, num_emits: int, homogeneous: bool = T
             seq_length - 1, num_node_blocks = num_node_blocks,
             dist = Categorical(num_cats = num_emits)
         )
+
+        if beta is not None:
+            assert beta.size(0) == num_latents and beta.size(1) == num_emits
+            ns_input.set_params(beta)
         
         ns_sum = None
         curr_zs = ns_input
@@ -46,6 +60,9 @@ def HMM(seq_length: int, num_latents: int, num_emits: int, homogeneous: bool = T
             
             if ns_sum is None:
                 ns = summate(curr_zs, num_node_blocks = num_node_blocks)
+                if alpha is not None:
+                    assert alpha.size(0) == num_latents and alpha.size(1) == num_latents
+                    ns.set_params(alpha)
                 ns_sum = ns
             else:
                 ns = ns_sum.duplicate(curr_zs, tie_params = homogeneous)
@@ -53,5 +70,9 @@ def HMM(seq_length: int, num_latents: int, num_emits: int, homogeneous: bool = T
             curr_zs = multiply(curr_xs, ns)
             
         ns = summate(curr_zs, num_node_blocks = 1, block_size = 1)
+
+        if gamma is not None:
+            assert gamma.dim() == 1 and gamma.size(0) == num_latents
+            ns.set_params(gamma.unsqueeze(0))
     
     return ns
