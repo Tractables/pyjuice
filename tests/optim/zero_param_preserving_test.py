77188f23d7e447badf45ac9556267f2957f0bfaa
Anji Liu <anjiliu219@gmail.com>
2025-11-06 13:07:00 +0000

    more test cases

--- tests/optim/zero_param_preserving_test.py
+++ tests/optim/zero_param_preserving_test.py
@@ -99,6 +99,65 @@ def test_zero_param_preserving_rand():
     assert torch.all((alpha > 1e-12) | (alpha_new < 1e-12))
 
 
+@pytest.mark.slow
+def test_zero_param_preserving_slow():
+
+    device = torch.device("cuda:0")
+
+    for seq_length in (32, 128):
+        for num_latents in (32, 64, 128, 256, 512, 1024):
+            for num_emits in (256, 50257):
+
+                root_ns = juice.structures.HMM(
+                    seq_length = seq_length,
+                    num_latents = num_latents,
+                    num_emits = num_emits
+                )
+
+                num_node_blocks = root_ns.chs[0].chs[1].num_node_blocks
+                block_size = root_ns.chs[0].chs[1].block_size
+
+                root_ns.init_parameters()
+                alpha = root_ns.chs[0].chs[1].get_source_ns()._params.clone().reshape(
+                    num_node_blocks, num_node_blocks, block_size, block_size).permute(
+                        0, 2, 1, 3).reshape(num_latents, num_latents)
+                while True:
+                    mask = torch.rand(num_latents, num_latents) < 0.1
+                    if torch.all(mask.long().sum(dim = 1) > 0):
+                        break
+                alpha[mask] = 0.0
+                root_ns.chs[0].chs[1].set_params(alpha)
+
+                pc = juice.compile(root_ns)
+                pc.to(device)
+
+                x = torch.randint(0, num_emits, [64, seq_length], device = device)
+
+                lls = pc(x)
+                pc.backward(x, logspace_flows = True, allow_modify_flows = False)
+
+                pc.mini_batch_em(step_size = 1.0, keep_zero_params = True)
+
+                pc.update_parameters()
+
+                alpha_new = root_ns.chs[0].chs[1].get_source_ns()._params.clone().reshape(
+                    num_node_blocks, num_node_blocks, block_size, block_size).permute(
+                        0, 2, 1, 3).reshape(num_latents, num_latents)
+
+                assert torch.all((alpha > 1e-12) | (alpha_new < 1e-12))
+
+                pc.mini_batch_em(step_size = 0.1, step_size_rescaling = True, keep_zero_params = True)
+
+                pc.update_parameters()
+
+                alpha_new = root_ns.chs[0].chs[1].get_source_ns()._params.clone().reshape(
+                    num_node_blocks, num_node_blocks, block_size, block_size).permute(
+                        0, 2, 1, 3).reshape(num_latents, num_latents)
+
+                assert torch.all((alpha > 1e-12) | (alpha_new < 1e-12))
+
+
 if __name__ == "__main__":
     test_zero_param_preserving_col()
     test_zero_param_preserving_rand()
+    test_zero_param_preserving_slow()
