a5ba9381aa0fc6e0718b764eab06279baaa0aba5
Anji Liu <anjiliu219@gmail.com>
2026-03-21 06:21:40 +0000

    fix logic for locating external parameters

--- src/pyjuice/nodes/distributions/softevi_categorical.py
+++ src/pyjuice/nodes/distributions/softevi_categorical.py
@@ -56,8 +56,6 @@ def _prep_args_apply_fw_kernel(layer, kwargs):
     assert not layer.provided("fw_local_ids")
     n_block_size = max_power_of_2_factor(layer.n_block_size)
 
-    layer_num_nodes = layer._output_ind_range[1] - layer._output_ind_range[0]
-
     # prepare BLOCK_SIZE and TILE_SIZE_K
     if target_kwargs["has_ext_ids"]:
         TILE_SIZE_K = min(16, triton.next_power_of_2(num_cats))
@@ -74,6 +72,7 @@ def _prep_args_apply_fw_kernel(layer, kwargs):
 
     use_tensor_core = (TILE_SIZE_K >= 16) and (BLOCK_SIZE_B >= 16) and (BLOCK_SIZE_N >= 16) and not target_kwargs["has_ext_ids"]
 
+    layer_num_nodes = layer._output_ind_range[1] - layer._output_ind_range[0]
     grid = (triton.cdiv(batch_size, BLOCK_SIZE_B), triton.cdiv(layer_num_nodes, BLOCK_SIZE_N))
 
     target_kwargs["TILE_SIZE_K"] = TILE_SIZE_K
@@ -83,6 +82,30 @@ def _prep_args_apply_fw_kernel(layer, kwargs):
     target_kwargs["use_tensor_core"] = use_tensor_core
 
     return target_kwargs, grid
+
+
+def _condition_apply_bk_params_kernel(layer, kwargs):
+    return "soft_evidence_logp" in kwargs
+
+
+def _prep_args_apply_bk_params_kernel(layer, kwargs):
+    target_kwargs = dict()
+
+    batch_size = kwargs["batch_size"]
+
+    soft_evidence_logp = kwargs["soft_evidence_logp"]
+    assert soft_evidence_logp.size(0) == batch_size, "Batch size doesn't match in `soft_evidence_logp`."
+
+    ext_num_vars = soft_evidence_logp.size(1)
+    target_kwargs["ext_num_vars"] = ext_num_vars
+
+    num_cats = soft_evidence_logp.size(2)
+    target_kwargs["num_cats"] = num_cats
+
+    target_kwargs["soft_evidence_logp_ptr"] = soft_evidence_logp
+    target_kwargs["var_idmapping_ptr"] = layer.var_idmapping
+
+    return target_kwargs
 
 
 class SoftEvidenceCategorical(Distribution):
@@ -216,7 +239,7 @@ class SoftEvidenceCategorical(Distribution):
                 inpars = tl.load(in_catpars_ptr, mask = (mask_b[:,None,None], mask_c[:,None,:], mask_n[None,None,:]), other = 0.0) # [BLOCK_SIZE_B, TILE_SIZE_K, BLOCK_SIZE_N]
 
                 # Load the external parameters
-                expars = tl.load(expars + i * TILE_SIZE_K, mask = (mask_b[:,None] & mask_c[None,:]), other = 0.0) # [BLOCK_SIZE_B, TILE_SIZE_K]
+                expars = tl.load(expars_ptr + i * TILE_SIZE_K, mask = (mask_b[:,None] & mask_c[None,:]), other = 0.0) # [BLOCK_SIZE_B, TILE_SIZE_K]
 
                 params = expars[:,:,None] + tl.log(inpars)
                 params_max = tl.max(params, axis = 1)
@@ -265,16 +288,45 @@ class SoftEvidenceCategorical(Distribution):
                     maxval + tlmath.log1p(tl.exp(diff))
                 )
 
-        # Compute unnormalized log-probabilities
+        # Compute unnormalized logprobs
         data = tl.load(data_ptr + vid * batch_size + offsets_b, mask = mask_b, other = 0) # [BLOCK_SIZE_B]
 
         log_in_p = tl.load(params_ptr + s_pids[None,:] + data[:,None], mask = (mask_b[:,None] & mask_n[None,:]), other = 0.0).log() # [BLOCK_SIZE_B, BLOCK_SIZE_N]
 
-        ex_p_ptr = external_categorical_logps_ptr + \
-            offsets_b * (ext_num_vars * num_cats) + \
-            lvid * num_cats + \
-            data
-        log_ex_p = tl.load(ex_p_ptr, mask = mask_b, other = 0.0) # [BLOCK_SIZE_B]
+        if has_ext_ids:
+
+            # Ptrs pointing to external parameter indices
+            catids_ptr = soft_evidence_cat_ids_ptr + \
+                offsets_b[:,None] * (ext_num_vars * num_cats) + \
+                lvid * max_num_cats + \
+                tl.arange(0, TILE_SIZE_K)[None,:] # [BLOCK_SIZE_B, TILE_SIZE_K]
+
+            # Ptrs pointing to external parameters
+            expar_ptr = soft_evidence_logp_ptr + \
+                offsets_b * (ext_num_vars * num_cats) + \
+                lvid * max_num_cats # [BLOCK_SIZE_B]
+
+            log_ex_p = tl.zeros([BLOCK_SIZE_B], dtype = tl.float32) - float("inf")
+            for i in range(K_NUM_TILES):
+                mask_c = (i * TILE_SIZE_K + tl.arange(0, TILE_SIZE_K) < num_cats) # [TILE_SIZE_K]
+
+                # Load the category IDs from `soft_evidence_cat_ids`
+                catids = tl.load(catids_ptr + i * TILE_SIZE_K, mask = (mask_b[:,None] & mask_c[None,:]), other = 0) # [BLOCK_SIZE_B, TILE_SIZE_K]
+
+                # Find matching ids
+                is_match = (catids == data[:,None]).to(tl.int64) # [BLOCK_SIZE_B, TILE_SIZE_K]
+                match_ids = tl.sum(is_match * tl.arange(0, TILE_SIZE_K), axis = 1) # [BLOCK_SIZE_B]
+                has_match = (tl.sum(is_match, axis = 1) > 0) # [BLOCK_SIZE_B]
+
+                expar = tl.load(expar_ptr + i * TILE_SIZE_K + match_ids, mask = (mask_b & has_match), other = 0.0) # [BLOCK_SIZE_B]
+                log_ex_p = tl.where(has_match, expar, log_ex_p)
+
+        else:
+            ex_p_ptr = soft_evidence_logp_ptr + \
+                offsets_b * (ext_num_vars * num_cats) + \
+                lvid * num_cats + \
+                data
+            log_ex_p = tl.load(ex_p_ptr, mask = mask_b, other = 0.0) # [BLOCK_SIZE_B]
 
         # Final output logprob
         log_p = log_in_p + log_ex_p[:,None] - logZ
